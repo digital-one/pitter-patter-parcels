14f760f49bb1da962a1377a5e4a2ce3359676282
digital-one <neil@digital-one.co.uk>
2014-07-09 21:26:43 +0100

    Directory setup
    Signed-off-by: digital-one <neil@digital-one.co.uk>

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-index page
+<!doctype html>
+<html class="no-js page-home logged ">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        
+        <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
+        <link rel="stylesheet" href="/css/main.css">
+                <script src="/js/vendor/modernizr-2.8.0.min.js"></script>
+        <!--[if lte IE 9]>
+            <link rel="stylesheet" href="/css/ie.css">
+            <script src="/js/vendor/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+
+
+    </body>
+    </html>
