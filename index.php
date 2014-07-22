1764b9c7382efc5f0d11d2390f6f33b6b6dde7be
digital-one <neil@digital-one.co.uk>
2014-07-22 14:04:55 +0100

    Updated files

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         
         <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
-        <link rel="stylesheet" href="/css/main.css">
+        <link rel="stylesheet" href="/css/layout.css">
+        <link href='http://fonts.googleapis.com/css?family=Bitter:400,700' rel='stylesheet' type='text/css'>
                 <script src="/js/vendor/modernizr-2.8.0.min.js"></script>
         <!--[if lte IE 9]>
             <link rel="stylesheet" href="/css/ie.css">
@@ -16,7 +17,32 @@
         <![endif]-->
     </head>
     <body>
+<!--header-->
+<header id="header" class="container">
+    <form id="search-frm" method="post" action="/"><input type="text" name="s" id="s" placeholder="Search" /></form>
+    <h1 id="home-link"><img src="images/pitter-patter-parcels.png" class="fit-grid" alt="Pitter Patter Parcels" /></h1>
+</header>
+<nav id="nav" class="container"><ul><li class="current-menu-item"><a href="">Home</a></li><li><a href="">Parcels</a></li><li><a href="">About us</a></li><li><a href="">Packaging</a></li><li><a href="">FAQs</a></li><li><a href="">Contact</a></li></ul></nav>
+<!--/header-->
+<!--main-->
+<div id="main" role="main" class="container">
+<section id="slider">
+<div class="slide"></div>
+</section>
+<!--category links-->
+<section id="category-links">
+<div class="column"><nav id="by-price"><h3>Parcels by price</h3><ul><li><a href="">&pound;25 parcels</a></li><li><a href="">&pound;40 parcels</a></li><li><a href="">&pound;60 parcels</a></li></ul></nav></div>
+<div class="column"><nav id="by-gender"><h3>Parcels by gender</h3><ul><li><a href="">Boys</a></li><li><a href="">Girls</a></li><li><a href="">Unisex</a></li></ul></nav></div>
+<div class="column"><nav id="by-you"><h3>Parcels by you</h3><ul><li><a href="">Build a box</a></li><li><a href="">Individual items</a></li><li><a href="">Personalised</a></li></ul></nav></div>
+</section>
+<!--/category links-->
+<!--parcels-->
+<section id="parcels">
 
+</section>
 
+<!--/parcels-->
+</div>
+<!--/main-->
     </body>
     </html>
