12c753da1b221c5ebc7be3386ad8b885f4400f61
digital-one <neil@digital-one.co.uk>
2014-07-23 21:50:10 +0100

    CSS build

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
         <link rel="stylesheet" href="/css/layout.css">
         <link href='http://fonts.googleapis.com/css?family=Bitter:400,700' rel='stylesheet' type='text/css'>
-                <script src="/js/vendor/modernizr-2.8.0.min.js"></script>
+                <script src="js/modernizr.js"></script>
         <!--[if lte IE 9]>
             <link rel="stylesheet" href="/css/ie.css">
             <script src="/js/vendor/respond.min.js"></script>
@@ -21,28 +21,41 @@
 <header id="header" class="container">
     <form id="search-frm" method="post" action="/"><input type="text" name="s" id="s" placeholder="Search" /></form>
     <h1 id="home-link"><img src="images/pitter-patter-parcels.png" class="fit-grid" alt="Pitter Patter Parcels" /></h1>
+    <div id="basket-summary"><a href=""><img src="images/basket-icon.png" class="fit-grid" /></a><span>1 item &pound;25.00</span></div>
 </header>
 <nav id="nav" class="container"><ul><li class="current-menu-item"><a href="">Home</a></li><li><a href="">Parcels</a></li><li><a href="">About us</a></li><li><a href="">Packaging</a></li><li><a href="">FAQs</a></li><li><a href="">Contact</a></li></ul></nav>
 <!--/header-->
 <!--main-->
 <div id="main" role="main" class="container">
 <section id="slider">
-<div class="slide"></div>
+<div class="slide">eopeopeopeop</div>
 </section>
 <!--category links-->
 <section id="category-links">
-<div class="column"><nav id="by-price"><h3>Parcels by price</h3><ul><li><a href="">&pound;25 parcels</a></li><li><a href="">&pound;40 parcels</a></li><li><a href="">&pound;60 parcels</a></li></ul></nav></div>
-<div class="column"><nav id="by-gender"><h3>Parcels by gender</h3><ul><li><a href="">Boys</a></li><li><a href="">Girls</a></li><li><a href="">Unisex</a></li></ul></nav></div>
-<div class="column"><nav id="by-you"><h3>Parcels by you</h3><ul><li><a href="">Build a box</a></li><li><a href="">Individual items</a></li><li><a href="">Personalised</a></li></ul></nav></div>
+<div class="column"><nav id="by-price"><h3>Parcels by price</h3><ul><li><a href="" class="button salmon"><span class="button-inner">&pound;25 parcels<span class="icon"><img src="images/icon-25-pound-parcels.png" class="fit-grid" /></span></span></a></li><li><a href="" class="button"><span class="button-inner">&pound;40 parcels<span class="icon"><img src="images/icon-40-pound-parcels.png" class="fit-grid" /></span></span></a></li><li><a href="" class="button pink"><span class="button-inner">&pound;60 parcels<span class="icon"><img src="images/icon-60-pound-parcels.png" class="fit-grid" /></span></span></a></li></ul></nav></div>
+<div class="column"><nav id="by-gender"><h3>Parcels by gender</h3><ul><li><a href="" class="button"><span class="button-inner">Boys<span class="icon"><img src="images/icon-boys.png" class="fit-grid" /></span></span></a></li><li><a href="" class="pink button"><span class="button-inner">Girls<span class="icon"><img src="images/icon-girls.png" class="fit-grid" /></span></span></a></li><li><a href="" class="green button"><span class="button-inner">Unisex<span class="icon"><img src="images/icon-unisex.png" class="fit-grid" /></span></span></a></li></ul></nav></div>
+<div class="column"><nav id="by-you"><h3>Parcels by you</h3><ul><li><a href="" class="green button"><span class="button-inner">Build a box<span class="icon"><img src="images/icon-40-pound-parcels.png" class="fit-grid" /></span></span></a></li><li><a href="" class="pink button"><span class="button-inner">Individual items<span class="icon"><img src="images/icon-individual-items.png" class="fit-grid" /></span></span></a></li><li><a href="" class="green button"><span class="button-inner">Personalised Door Hangers<span class="icon"><img src="images/icon-door-hangers.png" class="fit-grid" /></span></span></a></li></ul></nav></div>
 </section>
 <!--/category links-->
 <!--parcels-->
 <section id="parcels">
-
+<h2>Our Parcels</h2>
+<p>All our parcels come in three sizes so you can select<br />the perfect gift for that new family.</p>
+<article><figure><img src="images/img-little-pirate.jpg" class="fit-grid" /></figure><h3>Little Pirate</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec condimentum vulputate sagittis. </p><a href="" class="button"><span class="button-inner">Have a look inside<span class="icon"><img src="images/icon-look.png" class="fit-grid" /></span></span></a></article>
+<article><figure><img src="images/img-little-pirate.jpg" class="fit-grid" /></figure><h3>Little Pirate</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec condimentum vulputate sagittis. </p><a href="" class="pink button"><span class="button-inner">Have a look inside<span class="icon"><img src="images/icon-look.png" class="fit-grid" /></span></span></a></article>
+<article><figure><img src="images/img-little-pirate.jpg" class="fit-grid" /></figure><h3>Little Pirate</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec condimentum vulputate sagittis. </p><a href="" class="green button"><span class="button-inner">Have a look inside<span class="icon"><img src="images/icon-look.png" class="fit-grid" /></span></span></a></article>
+<article><figure><img src="images/img-little-pirate.jpg" class="fit-grid" /></figure><h3>Little Pirate</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec condimentum vulputate sagittis. </p><a href="" class="salmon button"><span class="button-inner">Have a look inside<span class="icon"><img src="images/icon-look.png" class="fit-grid" /></span></span></a></article>
 </section>
-
 <!--/parcels-->
+<!--footer-->
+<footer id="footer" class="container">
+<nav id="cta-links"><ul><li><a href="">Follow us on Facebook<span class="icon"><img src="images/icon-facebook.png" class="fit-grid" /></span></a></li><li><a href="">Free Shipping!<span class="icon"><img src="images/icon-shipping.png" class="fit-grid" /></a></li><li><a href="">Personalisation<span class="icon"><img src="images/icon-personalisation.png" class="fit-grid" /></a></li></ul></nav>
+<div id="contacts"><p><a href="mailto:info@pitterpatterparcels.co.uk">info@pitterpatterparcels.co.uk</a></p><p>Tel: <a href="tel:01132456789">01132-456-7890</a><br />Fax: 01132-456-7890</p></div>
+<nav id="footer-links"><ul><li><a href="">Privacy Policy</a></li><li><a href="">Terms &amp; Conditions</a></li></ul></nav>
+<span class="footer-icon"><img src="images/footer-feet.jpg" class="fit-grid" /></span>
+</footer>
+<!--/footer-->
 </div>
 <!--/main-->
-    </body>
-    </html>
+</body>
+</html>
