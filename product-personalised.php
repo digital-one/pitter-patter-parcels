5edd67c7fa67c66321fe3e3036dc0474bdda747f
digital-one <neil@digital-one.co.uk>
2014-08-05 22:56:48 +0100

    Personalised product page

---
+++ product-personalised.php
@@ -0,0 +1,79 @@
+<?php include_once('header.html'); ?>
+<!--content-->
+<div class="container">
+<div id="wrap" class="clearfix">
+    <div id="content">
+<aside>
+<nav id="sub-nav">
+<h3>Parcels by price</h3>
+<ul>
+    <li><a href="" class="salmon button"><span class="button-inner">&pound;25 parcels<span class="icon"><img src="images/icon-25-pound-parcels.png" class="fit-grid" /></span></span></a></li>
+    <li><a href="" class="button"><span class="button-inner">&pound;40 parcels<span class="icon"><img src="images/icon-40-pound-parcels.png" class="fit-grid" /></span></span></a></li>
+    <li><a href="" class="pink button"><span class="button-inner">&pound;60 parcels<span class="icon"><img src="images/icon-60-pound-parcels.png" class="fit-grid" /></span></span></a></li>
+</ul>
+<h3>Parcels by gender</h3>
+<ul>
+    <li><a href="" class="button"><span class="button-inner">Boys<span class="icon"><img src="images/icon-boys.png" class="fit-grid" /></span></span></a></li>
+    <li><a href="" class="pink button"><span class="button-inner">Girls<span class="icon"><img src="images/icon-girls.png" class="fit-grid" /></span></span></a></li>
+    <li><a href="" class="green button"><span class="button-inner">Unisex<span class="icon"><img src="images/icon-unisex.png" class="fit-grid" /></span></span></a></li>
+</ul>
+<h3>Parcels by you</h3>
+<ul>
+    <li><a href="" class="green button"><span class="button-inner">Build a box<span class="icon"><img src="images/icon-40-pound-parcels.png" class="fit-grid" /></span></span></a></li>
+    <li><a href="" class="pink button"><span class="button-inner">Individual items<span class="icon"><img src="images/icon-individual-items.png" class="fit-grid" /></span></span></a></li>
+    <li><a href="" class="salmon button"><span class="button-inner">Personalised<span class="icon"><img src="images/icon-door-hangers.png" class="fit-grid" /></span></span></a></li>
+</ul>
+</nav>
+</aside>
+<!--main-->
+<div id="main" role="main" class="blue">
+    <div id="inner">
+    <header id="category-header">
+    <h1>Personalised items</h1>
+    <h2>Personalised Door Hanger</h2>
+       <p>Arr harr me hearties. A perfect box for a perfect little pirate.</p>
+</header>
+
+   <div id="product" class="clearfix">
+   <div class="image"><figure><img src="images/star-door-hanger-tn.jpg" class="fit-grid" /></figure>
+    <form method="post" action="" id="add-to-basket">
+        <ul>
+            <li>
+                <input type="text" name="child-name" id="child-name" maxchars="25" placeholder="Enter Child's Name" /></li>
+                <li>
+                <input type="text" name="child-dob" id="child-dob" maxchars="25" placeholder="Enter Child's Date of Birth" /></li>
+        <li class="qty"><input type="text" name="qty" value="1" /><button type="submit" class="blue button"><span class="button-inner">add item to basket<span class="small icon"><img src="images/plus-icon.png" class="fit-grid" /></span></span></button></li></ul></form></div>
+   <div class="box-contents"><h5>Details</h5><p>Beautiful bright unisex newborn baby clothes gift set by design Scandinavian brand Maxomorra. Have your newborn baby stand out from the crowd with this gorgeous bright hat and sleep suit set.</p>
+
+<p>Purple pomegranate design suitable for boys or girls, to fit age 0-3 months.</p>
+
+<p>Add one today to one of our newborn baby gift boxes, available with UK standard or express delivery. Also European and Worldwide options available. All our gifts come hand gift wrapped by us, and delivered direct to your new parents. Shop for yours today and send your new parents a real treat!</p>
+
+
+
+
+
+</div>
+  
+</div>
+<section id="related">
+<h3>Still not enough? Add more to your box</h3>
+<div>
+ <article><figure><a href=""><img src="images/star-door-hanger-tn.jpg" class="fit-grid" /></a></figure><h5><a href="">Personalised Star Door Hanger</a></h5><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec condimentum vulputate sagittis. </p><a href="" class="blue button"><span class="button-inner">Add product to box<span class="small icon">
+<img class="fit-grid" src="images/plus-icon.png">
+</span></span></a></article>
+<article><figure><a href=""><img src="images/stripy-onesie-tn.jpg" class="fit-grid" /></a></figure><h5><a href="">Stripy Onesie</a></h5><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec condimentum vulputate sagittis. </p><a href="" class="blue button"><span class="button-inner">Add product to box<span class="small icon">
+<img class="fit-grid" src="images/plus-icon.png">
+</span></span></a></article>
+<article><figure><a href=""><img src="images/eggbox-socks-tn.jpg" class="fit-grid" /></a></figure><h5><a href="">Eggbox Socks</a></h5><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec condimentum vulputate sagittis. </p><a href="" class="blue button"><span class="button-inner">Add product to box<span class="small icon">
+<img class="fit-grid" src="images/plus-icon.png">
+</span></span></a></article>
+</div>
+    </section>
+
+</div>
+</div>
+
+<!--/main-->
+</div>
+<?php include_once('footer.html'); ?>
